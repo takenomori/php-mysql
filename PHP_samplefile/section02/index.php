a7a7932220f3612cc0a0b962cefcdb9461420fd7
takeru0922 <moriai0922takeru@gmail.com>
2020-07-28 20:56:11 +0900

    [add]準備

--- PHP_samplefile/section02/index.php
+++ PHP_samplefile/section02/index.php
@@ -1,25 +1,26 @@
 <!doctype html>
 <html lang="ja">
-<head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
 
-<title>PHP</title>
-</head>
-<body>
-<header>
-<h1 class="font-weight-normal">PHP</h1>    
-</header>
+    <title>PHP</title>
+    </head>
+    
+    <body>
+    <header>
+    <h1 class="font-weight-normal">PHP</h1>    
+    </header>
 
-<main>
-<h2>Practice</h2>
-<pre>
-<!-- ここにプログラムを記述します -->
-</pre>
-</main>
-</body>    
+    <main>
+    <h2>Practice</h2>
+    <pre>
+    <!-- ここにプログラムを記述します -->
+    </pre>
+    </main>
+    </body>    
 </html>
