46bec5a1f9649bf57fc9741ed4edee52b35b9dcd
takeru0922 <moriai0922takeru@gmail.com>
2020-08-03 22:24:09 +0900

    [update]更新

--- PHP_samplefile/section02/sample01.php
+++ PHP_samplefile/section02/sample01.php
@@ -1,27 +1,27 @@
 <!doctype html>
 <html lang="ja">
-<head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/style.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/style.css">
 
-<title>PHP</title>
-</head>
-<body>
-<header>
-<h1 class="font-weight-normal">PHP</h1>    
-</header>
+        <title>PHP</title>
+    </head>
+    <body>
+        <header>
+            <h1 class="font-weight-normal">PHP</h1>    
+        </header>
 
-<main>
-<h2>Practice</h2>
-<pre>
-<?php
-print("I'm studying \"PHP\"");
-?>
-</pre>
-</main>
-</body>    
+        <main>
+            <h2>Practice</h2>
+            <pre>
+            <?php
+            print("I'm studying \"PHP\"");
+            ?>
+            </pre>
+        </main>
+    </body>    
 </html>
